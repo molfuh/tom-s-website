23abd2aa0225177fc3031f691b8b5c93dbc496a9
Molly Fuhrman <mollyfuhrman@Mollys-Air.lan1>
2021-03-07 13:03:05 -0800

    Div transitions upon page render.

--- src/components/FirstView.jsx
+++ src/components/FirstView.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 import SecondView from './SecondView.jsx';
+// import React, { useState } from 'react';
+
 
 export default function FirstView({ coverPic }) {
+    const [transition, setTransition] = useState(false);
+    const handleTransition = () => {
+        setTransition(true);
+    }
     return (
         <>
             <FirstViewDiv coverPic={coverPic}>
-                {/* use sass to set up grid system for div? */}
-                <StyledDiv>
-                Composer, Producer, Performer
-                <br />
-                THOMAS M.L. RAVAGO
-                {/* Passionate and self-motivated musician, 
-                teacher, composer, producer, and performer, 
-                with a philosophy that has a huge emphasis on the power 
-                and importance of collaboration. Diligent and hard-working 
-                individual that strives to bring audiences a well-rounded 
-                musical experience, to promote positivity and camaraderie 
-                in collaborative work environments, and to continue learning 
-                and passing on all there is to learn as a once-and-always 
-                student of music. */}
+                {/* {transition && {handleTransition()} */}
+                <StyledDiv transition={transition}>
+                    Composer, Producer, Performer
+                    <br />
+                    THOMAS M.L. RAVAGO
+                    {/* Passionate and self-motivated musician, 
+                    teacher, composer, producer, and performer, 
+                    with a philosophy that has a huge emphasis on the power 
+                    and importance of collaboration. Diligent and hard-working 
+                    individual that strives to bring audiences a well-rounded 
+                    musical experience, to promote positivity and camaraderie 
+                    in collaborative work environments, and to continue learning 
+                    and passing on all there is to learn as a once-and-always 
+                    student of music. */}
                 </StyledDiv>
             </FirstViewDiv>
             <SecondView />
@@ -35,10 +41,19 @@ const FirstViewDiv = styled.div`
     position: absolute;
     height: 100vh;
     width: 100vw;
+    display: flex;
+    align-items:center;
+    justify-content:center;
+`;
+
+const visible = keyframes`
+  from {opacity: 0;}
+  to {opacity: 1;}
 `;
 
 const StyledDiv = styled.div`
-    position: relative;
+    opacity: 0;
+    position: absolute;
     color: white;
     padding: 20px;
     display: flex;
@@ -48,5 +63,5 @@ const StyledDiv = styled.div`
     height: 80vh;
     width: 25vw;
     left: 100px;
-    top: 100px;
+    animation: ${visible} 1s forwards 1s;
 `;
