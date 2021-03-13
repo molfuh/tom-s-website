0a8039f6c69589ba6424c36b7e63bebffcdd15f8
Molly Fuhrman <mollyfuhrman@gmail.com>
2021-03-13 12:43:09 -0800

    Set up react routing.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+// axe is undefined
+axe
+  .run()
+  .then(results => {
+    if (results.violations.length) {
+      throw new Error('Accessibility issues found');
+    }
+  })
+  .catch(err => {
+    console.error('Something bad happened:', err.message);
+  });
