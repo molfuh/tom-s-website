a3d9aba24009ce63847a75c79b8ed40b50bd7109
Molly Fuhrman <mollyfuhrman@gmail.com>
2021-03-14 21:20:14 -0700

    Added buttons and text to show up when clicked.

--- src/components/Listen.jsx
+++ src/components/Listen.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from './Footer';
 import styled from 'styled-components';
 import Tilt from 'react-tilt';
 
 export default function Listen() {
+  const [button1, clickButton1] = useState(false)
+  const [button2, clickButton2] = useState(false)
   return (
     <>
     {/* container */}
@@ -14,18 +16,21 @@ export default function Listen() {
         <div>
           <StyledLeft>
             <Tilt className="Tilt" options={{ max : 25 }} >
-              {/* <div className="Tilt-inner"> 👽 </div> */}
-              <button>Music Composition</button>
+              <>
+              <button onClick={() => { clickButton1(!button1) }}>Music Composition</button>
+              </>
             </Tilt>
           </StyledLeft>
           <StyledRight>
           <Tilt className="Tilt" options={{ max : 25 }} >
-              {/* <div className="Tilt-inner"> 👽 </div> */}
-              <button>Music Production</button>
+              <button onClick={() => { clickButton2(!button2) }}>Music Production</button>
             </Tilt>
           </StyledRight>
         </div>
       </Container>
+      <Composition button1={button1}>Composition Works:
+      <div>Recording 1</div></Composition>
+      <Production button2={button2}>Production Works:</Production>
     </>
   )
 }
@@ -48,6 +53,7 @@ const StyledLeft = styled.div`
   grid-column-start: 1;
   margin: 10vw;
   button {
+    border-radius: 10px;
     background-color: red;
     padding: 20px;
     height: 20vw;
@@ -63,4 +69,22 @@ const StyledRight = styled(StyledLeft)`
   button {
     background-color: yellow;
   }
+`;
+
+const Composition = styled.div`
+  position: absolute;
+  opacity: ${ props => props.button1 ? 1 : 0 };
+  /* visibility: ${ props => props.button1 ? 'visible' : 'hidden' }; */
+  transition: opacity 1s;
+  z-index: 1;
+  /* background-color: white; */
+  left: 50%;
+  transform: translate(-50%, 0);
+  font-size: 2rem;
+`;
+
+const Production = styled(Composition)`
+  opacity: ${ props => props.button2 ? 1 : 0 };
+  /* visibility: ${ props => props.button2 ? 'visible' : 'hidden' }; */
+  /* transition: opacity 1s; */
 `;
