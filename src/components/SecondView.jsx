5269338ddad9204df362f41b761000e56276444c
Molly Fuhrman <mollyfuhrman@gmail.com>
2021-04-15 11:55:45 -0700

    Click on logo to go back to home, adjusted font sizes in header.

--- src/components/SecondView.jsx
+++ src/components/SecondView.jsx
@@ -14,7 +14,9 @@ export default function SecondView({ tom_s_logo }) {
                         setDivVis(isVisible);
                     }}>
                     <StyledDiv divVis={divVis} >
-                      <p><span className="summary">SUMMARY</span> <br /> Passionate and self-motivated musician, teacher, composer, producer, and performer, with a philosophy that has a huge emphasis on the power and importance of collaboration. Diligent and hard-working individual that strives to bring audiences a well-rounded musical experience, to promote positivity and camaraderie in collaborative work environments, and to continue learning and passing on all there is to learn as a once-and-always student of music.</p>
+                      <p><span className="summary">SUMMARY</span> <br />
+                        <strong>Passionate</strong> and <strong>self-motivated</strong> musician, teacher, composer, producer, and performer, with a philosophy that has a huge emphasis on the <strong>power</strong> and <strong>importance</strong> of <strong>collaboration</strong>. <strong>Diligent</strong> and <strong>hard-working</strong> individual that strives to bring audiences a well-rounded musical experience, to promote <strong>positivity</strong> and <strong>camaraderie</strong> in collaborative work environments, and to continue learning and passing on all there is to learn as a <br/> <strong>once-and-always student of music</strong>.
+                      </p>
                     </StyledDiv>
                 </VizSensor>
             </SecondViewDiv>
